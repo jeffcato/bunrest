6b6ba90cdbc672e73a8e79dd4ec06ec62e9546be
Jeff Cato <sayheycato@gmail.com>
2024-08-05 15:14:31 -0600

    Response onFinish

--- src/server/response.ts
+++ src/server/response.ts
@@ -1,63 +1,78 @@
 export class BunResponse {
     private response: Response;
     private options: ResponseInit = {};
+    private subs: Array<any> = [];
 
     status(code: number): BunResponse {
-        this.options.status = code;
-        return this;
+      this.options.status = code;
+      return this;
     }
 
     option(option: ResponseInit): BunResponse {
-        this.options = Object.assign(this.options, option);
-        return this;
+      this.options = Object.assign(this.options, option);
+      return this;
     }
 
     statusText(text: string): BunResponse {
-        this.options.statusText = text;
-        return this;
+      this.options.statusText = text;
+      return this;
     }
 
     json(body: any): void {
-        this.response = Response.json(body, this.options);
+      const that = this
+      that.response = Response.json(body, that.options);
+      that.subs.filter(({type}) => type === 'finished')
+        .forEach(({f}) => f(that.response))
     }
 
     send(body: any): void {
-        this.response = new Response(body, this.options);
+      const that = this
+      that.response = new Response(body, that.options);
+      that.subs.filter(({type}) => type === 'finished')
+        .forEach(({f}) => f(that.response))
     }
 
     redirect(url: string, status: number = 302): void {
-        this.response = Response.redirect(url, status);
+      const that = this
+      that.response = Response.redirect(url, status);
+      that.subs.filter(({type}) => type === 'finished')
+        .forEach(({f}) => f(that.response))
     }
 
     // nodejs way to set headers
     setHeader(key: string, value: any) {
-        if (!key || !value) {
-            throw new Error('Headers key or value should not be empty');
-        }
+      if (!key || !value) {
+          throw new Error('Headers key or value should not be empty');
+      }
 
-        const headers = this.options.headers;
-        if (!headers) {
-            this.options.headers = { keys: value };
-        }
-        this.options.headers[key] = value;
-        return this;
+      const headers = this.options.headers;
+      if (!headers) {
+          this.options.headers = { keys: value };
+      }
+      this.options.headers[key] = value;
+      return this;
     }
 
     // nodejs way to get headers
     getHeader() {
-        return this.options.headers;
+      return this.options.headers;
     }
 
     headers(header: HeadersInit): BunResponse {
-        this.options.headers = header;
-        return this;
+      this.options.headers = header;
+      return this;
     }
 
     getResponse(): Response {
-        return this.response;
+      return this.response;
     }
 
     isReady(): boolean {
-        return !!this.response;
+      return !!this.response;
+    }
+
+    on(type, f): Response {
+      this.subs.push({type, f})
+      return this
     }
 }
